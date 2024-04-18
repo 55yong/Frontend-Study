374af10279582f6ebd599d95d8eeffc69ff2be1d
55yong <dhdydwns988@gmail.com>
2024-04-18 15:28:57 +0900

    하나 빼먹었누 ㅋㅋ

--- 용준/04주차-React/todo-app/src/components/TodoInsert.js
+++ 용준/04주차-React/todo-app/src/components/TodoInsert.js
@@ -22,7 +22,7 @@ const TodoInsert = ({ onInsert }) => {
   );
 
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요"
         value={value}
