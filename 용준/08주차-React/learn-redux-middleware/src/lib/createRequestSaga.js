35d89aec1f8b79ffb5680a1f9c3f072013004b8d
55yong <dhdydwns988@gmail.com>
2024-05-25 20:55:40 +0900

    안쓰는놈들 없애기

--- 용준/08주차-React/learn-redux-middleware/src/lib/createRequestSaga.js
+++ 용준/08주차-React/learn-redux-middleware/src/lib/createRequestSaga.js
@@ -1,5 +1,5 @@
 import { call, put } from "redux-saga/effects";
-import { startLoading, finishLoading } from "../modules/loading";
+import { startLoading } from "../modules/loading";
 
 export default function createRequestSaga(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
