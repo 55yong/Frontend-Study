316ce7e004746e6475de8befc1b4a211b7da0419
55yong <dhdydwns988@gmail.com>
2024-05-26 21:40:39 +0900

    스플리팅

--- 용준/08주차-React/ssr-recipe/src/App.js
+++ 용준/08주차-React/ssr-recipe/src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Menu from "./components/Menu";
-import RootPage from "./pages/RootPage";
-import RedPage from "./pages/RedPage";
-import BluePage from "./pages/BluePage";
-import UsersPage from "./pages/UsersPage";
+import loadable from "@loadable/component";
+
+const RootPage = loadable(() => import("./pages/RootPage"));
+const RedPage = loadable(() => import("./pages/RedPage"));
+const BluePage = loadable(() => import("./pages/BluePage"));
+const UsersPage = loadable(() => import("./pages/UsersPage"));
 
 const App = () => {
   return (
