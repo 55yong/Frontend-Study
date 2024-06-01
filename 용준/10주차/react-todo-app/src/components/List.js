368d94e5ec37a624743e55c702e9d2edf57e94b6
55yong <dhdydwns988@gmail.com>
2024-06-01 22:46:27 +0900

    수정 기능 추가

--- 용준/10주차/react-todo-app/src/components/List.js
+++ 용준/10주차/react-todo-app/src/components/List.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = React.memo(
   ({
@@ -13,6 +13,9 @@ const List = React.memo(
   }) => {
     console.log("List is Rendering");
 
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
     const handleCompleteChange = (id) => {
       let newTodoData = todoData.map((data) => {
         if (data.id === id) {
@@ -23,43 +26,102 @@ const List = React.memo(
       setTodoData(newTodoData);
     };
 
-    return (
-      <div
-        key={id}
-        {...provided.draggableProps}
-        ref={provided.innerRef}
-        {...provided.dragHandleProps}
-      >
-        <div
-          className={`${
-            snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-          } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-        >
-          <div>
-            <input
-              type="checkbox"
-              onChange={() => handleCompleteChange(id)}
-              defaultChecked={completed}
-            />
-            <span
-              className={
-                completed ? "line-through px-2 text-lg" : "px-2 text-lg"
-              }
-            >
-              {title}
-            </span>
-          </div>
-          <div>
-            <button
-              className="px-4 py-2 float-right text-black border-black border rounded hover:text-white hover:border-red-500 hover:bg-red-500"
-              onClick={() => handleClick(id)}
-            >
-              x
-            </button>
+    const handleEditChange = (e) => {
+      setEditedTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.title = editedTitle;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+      setIsEditing(false);
+    };
+
+    if (isEditing) {
+      return (
+        <div>
+          <div
+            className={
+              "bg-gray-100 flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded"
+            }
+          >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <input
+                  value={editedTitle}
+                  onChange={handleEditChange}
+                  className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+                />
+              </form>
+            </div>
+            <div>
+              <button
+                className="px-4 py-2 float-right text-black border-black border rounded hover:text-white hover:border-red-500 hover:bg-red-500"
+                onClick={() => setIsEditing(false)}
+              >
+                x
+              </button>
+              <button
+                className="px-4 py-2 float-right text-black border-black border rounded hover:text-white hover:border-red-500 hover:bg-red-500"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                SAVE
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div
+          key={id}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+        >
+          <div
+            className={`${
+              snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+            } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+          >
+            <div>
+              <input
+                type="checkbox"
+                onChange={() => handleCompleteChange(id)}
+                defaultChecked={completed}
+              />
+              <span
+                className={
+                  completed ? "line-through px-2 text-lg" : "px-2 text-lg"
+                }
+              >
+                {title}
+              </span>
+            </div>
+            <div>
+              <button
+                className="px-4 py-2 float-right text-black border-black border rounded hover:text-white hover:border-red-500 hover:bg-red-500"
+                onClick={() => handleClick(id)}
+              >
+                x
+              </button>
+              <button
+                className="px-4 py-2 float-right text-black border-black border rounded hover:text-white hover:border-red-500 hover:bg-red-500"
+                onClick={() => setIsEditing(true)}
+              >
+                EDIT
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 );
 
