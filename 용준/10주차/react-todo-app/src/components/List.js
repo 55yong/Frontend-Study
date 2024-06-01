928c4bba213a4fc3e6c9da35b0d51fe44fab3edf
55yong <dhdydwns988@gmail.com>
2024-06-01 20:22:19 +0900

    List 컴포넌트 생성

--- 용준/10주차/react-todo-app/src/components/List.js
+++ 용준/10주차/react-todo-app/src/components/List.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
-export default function List({ todoData, setTodoData }) {
+function List({
+  id,
+  title,
+  completed,
+  todoData,
+  setTodoData,
+  provided,
+  snapshot,
+}) {
   const handleClick = (id) => {
     let newTodoData = todoData.filter((data) => data.id !== id);
     console.log("newTodoData", newTodoData);
@@ -18,77 +25,41 @@ export default function List({ todoData, setTodoData }) {
     setTodoData(newTodoData);
   };
 
-  const handleEnd = (result) => {
-    console.log(result);
-
-    if (!result.destination) return;
-
-    const newTodoData = todoData;
-
-    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
-
-    newTodoData.splice(result.destination.index, 0, reorderedItem);
-    setTodoData(newTodoData);
-  };
-
   return (
-    <div>
-      <DragDropContext onDragEnd={handleEnd}>
-        <Droppable droppableId="todo">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {todoData.map((data, index) => (
-                <Draggable
-                  key={data.id}
-                  draggableId={data.id.toString()}
-                  index={index}
-                >
-                  {(provided, snapshot) => (
-                    <div
-                      key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                    >
-                      <div
-                        className={`${
-                          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                      >
-                        <div>
-                          <input
-                            type="checkbox"
-                            onChange={() => handleCompleteChange(data.id)}
-                            defaultChecked={data.completed}
-                          />
-                          <span
-                            className={
-                              data.completed
-                                ? "line-through px-2 text-lg"
-                                : "px-2 text-lg"
-                            }
-                          >
-                            {data.title}
-                          </span>
-                        </div>
-                        <div>
-                          <button
-                            className="px-4 py-2 float-right text-black border-black border rounded hover:text-white hover:border-red-500 hover:bg-red-500"
-                            onClick={() => handleClick(data.id)}
-                          >
-                            x
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div
+      key={id}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+    >
+      <div
+        className={`${
+          snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+      >
+        <div>
+          <input
+            type="checkbox"
+            onChange={() => handleCompleteChange(id)}
+            defaultChecked={completed}
+          />
+          <span
+            className={completed ? "line-through px-2 text-lg" : "px-2 text-lg"}
+          >
+            {title}
+          </span>
+        </div>
+        <div>
+          <button
+            className="px-4 py-2 float-right text-black border-black border rounded hover:text-white hover:border-red-500 hover:bg-red-500"
+            onClick={() => handleClick(id)}
+          >
+            x
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default List;
