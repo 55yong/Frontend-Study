52a7d02d16fb4dc9cd8794fdb30ec43b0fcc1831
55yong <dhdydwns988@gmail.com>
2024-06-05 02:47:29 +0900

    footer 작업 완료

--- 용준/10주차/react-netflix-clone/src/App.js
+++ 용준/10주차/react-netflix-clone/src/App.js
@@ -3,6 +3,7 @@ import Banner from "./components/Banner";
 import Nav from "./components/Nav";
 import Row from "./components/Row";
 import requests from "./api/requests";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
         id="DM"
         fetchUrl={requests.fetchDocumentaries}
       />
+      <Footer />
     </div>
   );
 }
