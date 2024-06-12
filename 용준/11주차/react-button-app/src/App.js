87f7bf103ddd0a149b18aeb078856370d02e12a0
55yong <dhdydwns988@gmail.com>
2024-06-13 03:37:28 +0900

    뭔가 했어!!

---
+++ 용준/11주차/react-button-app/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testid="counter" style={{ color: "red" }}>
+          {count}
+        </h3>
+        <div>
+          <button
+            data-testid="plus-button"
+            disabled={disabled}
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            +
+          </button>
+          <button
+            data-testid="minus-button"
+            disabled={disabled}
+            onClick={() => setCount((prev) => prev - 1)}
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <button
+            style={{ backgroundColor: "blue" }}
+            data-testid="on/off-button"
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
